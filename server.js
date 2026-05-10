a476f1278f8de1e12636a016c4ebbe65e35e252c
Amir <arzeroomchi@gmail.com>
2026-05-09 23:46:27 -0700

    Fix home route

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
+});
 app.use("/uploads", express.static("uploads"));
 
 /* ---------------- MONGODB ---------------- */
